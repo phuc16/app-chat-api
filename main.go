214c7df9edb02e4b08838ca4bfe08a274f0b55b5
Tien Le <lengoctienbrvt@gmail.com>
2024-03-16 19:00:09 +0700

    connect db

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_NAME")
+
+	// Connection string
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database)
+
+	// Connect to the database
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Connected to the MySQL database!")
+
+	// Perform database operations here...
+	fmt.Println(db.Exec(`SHOW TABLE;`))
 
 	e := echo.New()
 
